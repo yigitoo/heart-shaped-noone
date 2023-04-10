e206a0e736b7a1d3e4436c7fe14947694d39c604
Yiğit GÜMÜŞ <rawns0909@gmail.com>
2023-04-10 20:55:06 +0300

    .

---
+++ zeynep.py
@@ -0,0 +1,10 @@
+
+print('\n'.join
+       ([''.join
+         ([('zeynep'[(x-y)%5 ]
+           if((x*0.05)**2+(y*0.1)**2-1)
+             **3-(x*0.05)**2*(y*0.1)
+               **3<=0 else' ')
+                 for x in range(-30,30)])
+                   for y in range(15,-15,-1)]))
+
